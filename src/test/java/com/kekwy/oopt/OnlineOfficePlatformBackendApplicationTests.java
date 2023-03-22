24e9613ebdee21427bb4c35941feb1ecf7203233
Kekwy <keekkewy@qq.com>
2023-03-22 21:42:41 +0800

    init

---
+++ src/test/java/com/kekwy/oopt/OnlineOfficePlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kekwy.oopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOfficePlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
